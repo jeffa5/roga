efe2c66641e4e9f22ce69fac136a01d4a190a62a
Jeffas <dev@jeffas.io>
2019-12-30 21:49:07 +0000

    Add scrolling dependencies

--- src/Main.elm
+++ src/Main.elm
@@ -9,7 +9,7 @@ import Http
 import Json.Decode exposing (Decoder, field, list, map5, string)
 import Random
 import Random.List
-import SmoothScroll exposing (defaultConfig, scrollTo, scrollToWithOptions)
+import SmoothScroll exposing (defaultConfig, scrollToWithOptions)
 import Task
 import Time exposing (Posix, toMinute, toSecond, utc)
 
